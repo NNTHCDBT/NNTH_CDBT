72cb0f67dfbacb408c9459d6e015f3d5273551d2
NNTHCDBT <nnth.cdbt2024@gmail.com>
2024-11-06 15:22:44 +0700

    Add files via upload

---
+++ T_Tho/course.php
@@ -0,0 +1,95 @@
+ 
+    <div class="container">
+        <div class="top">
+         <h1>Our Main Courses</h1>
+         <p>Rerum nam dolor, consectetur in efficitur sit, franchesca nec lorem</p>
+         <br>
+         <p id="red-line"></p>
+         <br>
+        </div>
+
+        <div class="cotlon">
+            <div class="cot">
+                <div class="media-container">
+                    <img class="media-image" height="200" src="T_Tho/img/sv3.jpg" width="300" />
+                    <video class="media-video" src="video/video_chen.mp4" autoplay muted loop></video>
+                </div>
+                <div class="cot-nd">
+                    <span class="category">Science</span>
+                    <h3>Electrical Engineering</h3>
+                    <p>Oras ultricies lacus consectetur, consectetur scelerisque arcu curabitur.</p>
+                    <a class="apply-btn" href="#">Apply Now</a>
+                </div>
+            </div>
+
+            <div class="cot">
+                <div class="media-container">
+                    <img class="media-image" height="200" src="T_Tho/img/sv3.jpg" width="300" />
+                    <video class="media-video" src="T_Tho/video/video_chen.mp4" autoplay muted loop></video>
+                </div>
+                <div class="cot-nd">
+                    <span class="category">Science</span>
+                    <h3>Electrical Engineering</h3>
+                    <p>Oras ultricies lacus consectetur, consectetur scelerisque arcu curabitur.</p>
+                    <a class="apply-btn" href="#">Apply Now</a>
+                </div>
+            </div>
+
+
+            <div class="cot">
+                <div class="media-container">
+                    <img class="media-image" height="200" src="T_Tho/img/sv3.jpg" width="300" />
+                    <video class="media-video" src="T_Tho/video/video_chen.mp4" autoplay muted loop></video>
+                </div>
+                <div class="cot-nd">
+                    <span class="category">Science</span>
+                    <h3>Electrical Engineering</h3>
+                    <p>Oras ultricies lacus consectetur, consectetur scelerisque arcu curabitur.</p>
+                    <a class="apply-btn" href="#">Apply Now</a>
+                </div>
+            </div>
+
+
+            <div class="cot">
+                <div class="media-container">
+                    <img class="media-image" height="200" src="T_Tho/img/sv3.jpg" width="300" />
+                    <video class="media-video" src="T_Tho/video/video_chen.mp4" autoplay muted loop></video>
+                </div>
+                <div class="cot-nd">
+                    <span class="category">Science</span>
+                    <h3>Electrical Engineering</h3>
+                    <p>Oras ultricies lacus consectetur, consectetur scelerisque arcu curabitur.</p>
+                    <a class="apply-btn" href="#">Apply Now</a>
+                </div>
+            </div>
+
+
+            <div class="cot">
+                <div class="media-container">
+                    <img class="media-image" height="200" src="T_Tho/img/sv3.jpg" width="300" />
+                    <video class="media-video" src="T_Tho/video/video_chen.mp4" autoplay muted loop></video>
+                </div>
+                <div class="cot-nd">
+                    <span class="category">Science</span>
+                    <h3>Electrical Engineering</h3>
+                    <p>Oras ultricies lacus consectetur, consectetur scelerisque arcu curabitur.</p>
+                    <a class="apply-btn" href="#">Apply Now</a>
+                </div>
+            </div>
+
+
+            <div class="cot">
+                <div class="media-container">
+                    <img class="media-image" height="200" src="T_Tho/img/sv3.jpg" width="300" />
+                    <video class="media-video" src="T_Tho/video/video_chen.mp4" autoplay muted loop></video>
+                </div>
+                <div class="cot-nd">
+                    <span class="category">Science</span>
+                    <h3>Electrical Engineering</h3>
+                    <p>Oras ultricies lacus consectetur, consectetur scelerisque arcu curabitur.</p>
+                    <a class="apply-btn" href="#">Apply Now</a>
+                </div>
+            </div>
+        </div>
+       </div>
+	 
---
+++ T_Tho/lecturers.php
@@ -0,0 +1,228 @@
+    <div class="khoangcach">
+        <h2>Đội ngũ giảng viên</h2>
+        
+        <div class="caccot">
+    
+         <div class="cot_t">
+          <a href="#" target="_blank">
+            <img height="300" width="200" src="T_Tho/img/cd_1.jpg" />
+            <div class="info">
+              <strong>Tên: John Doe</strong><br>
+              Ngày sinh: 01/01/1990<br>
+              Quê quán: Hà Nội
+            </div>
+          </a>
+          <h3>Rosy Janner</h3>
+          <p>Senior Finance Lecturer</p>
+          <div class="social-icons">
+           <a href="#">
+            <i class="fab fa-facebook-f"></i>
+           </a>
+           <a href="#">
+            <i class="fab fa-twitter"></i>
+           </a>
+           <a href="#">
+            <i class="fab fa-linkedin-in"></i>
+           </a>
+           <a href="#">
+            <i class="fab fa-google-plus-g"></i>
+           </a>
+          </div>
+         </div>
+     
+         <div class="cot_t">
+          <a href="#" target="_blank">
+            <img height="300" width="200" src="T_Tho/img/cd_1.jpg" />
+            <div class="info">
+              <strong>Tên: John Doe</strong><br>
+              Ngày sinh: 01/01/1990<br>
+              Quê quán: Hà Nội
+            </div>
+          </a>
+          <h3>Rosy Janner</h3>
+          <p>Senior Finance Lecturer</p>
+          <div class="social-icons">
+           <a href="#">
+            <i class="fab fa-facebook-f"></i>
+           </a>
+           <a href="#">
+            <i class="fab fa-twitter"></i>
+           </a>
+           <a href="#">
+            <i class="fab fa-linkedin-in"></i>
+           </a>
+           <a href="#">
+            <i class="fab fa-google-plus-g"></i>
+           </a>
+          </div>
+         </div>
+     
+         <div class="cot_t">
+          <a href="#" target="_blank">
+            <img height="300" width="200" src="T_Tho/img/cd_1.jpg" />
+            <div class="info">
+              <strong>Tên: John Doe</strong><br>
+              Ngày sinh: 01/01/1990<br>
+              Quê quán: Hà Nội
+            </div>
+          </a>
+          <h3>Rosy Janner</h3>
+          <p>Senior Finance Lecturer</p>
+          <div class="social-icons">
+           <a href="#">
+            <i class="fab fa-facebook-f"></i>
+           </a>
+           <a href="#">
+            <i class="fab fa-twitter"></i>
+           </a>
+           <a href="#">
+            <i class="fab fa-linkedin-in"></i>
+           </a>
+           <a href="#">
+            <i class="fab fa-google-plus-g"></i>
+           </a>
+          </div>
+         </div>
+     
+         <div class="cot_t">
+          <a href="#" target="_blank">
+            <img height="300" width="200" src="T_Tho/img/cd_1.jpg" />
+            <div class="info">
+              <strong>Tên: John Doe</strong><br>
+              Ngày sinh: 01/01/1990<br>
+              Quê quán: Hà Nội
+            </div>
+          </a>
+          <h3>Rosy Janner</h3>
+          <p>Senior Finance Lecturer</p>
+          <div class="social-icons">
+           <a href="#">
+            <i class="fab fa-facebook-f"></i>
+           </a>
+           <a href="#">
+            <i class="fab fa-twitter"></i>
+           </a>
+           <a href="#">
+            <i class="fab fa-linkedin-in"></i>
+           </a>
+           <a href="#">
+            <i class="fab fa-google-plus-g"></i>
+           </a>
+          </div>
+         </div>
+  </div>
+</div>
+
+
+<div class="khoangcach">
+  <div class="caccot">
+
+   <div class="cot_t">
+    <a href="#" target="_blank">
+      <img height="300" width="200" src="T_Tho/img/cd_1.jpg" />
+      <div class="info">
+        <strong>Tên: John Doe</strong><br>
+        Ngày sinh: 01/01/1990<br>
+        Quê quán: Hà Nội
+      </div>
+    </a>
+    <h3>Rosy Janner</h3>
+    <p>Senior Finance Lecturer</p>
+    <div class="social-icons">
+     <a href="#">
+      <i class="fab fa-facebook-f"></i>
+     </a>
+     <a href="#">
+      <i class="fab fa-twitter"></i>
+     </a>
+     <a href="#">
+      <i class="fab fa-linkedin-in"></i>
+     </a>
+     <a href="#">
+      <i class="fab fa-google-plus-g"></i>
+     </a>
+    </div>
+   </div>
+
+   <div class="cot_t">
+    <a href="#" target="_blank">
+      <img height="300" width="200" src="T_Tho/img/cd_1.jpg" />
+      <div class="info">
+        <strong>Tên: John Doe</strong><br>
+        Ngày sinh: 01/01/1990<br>
+        Quê quán: Hà Nội
+      </div>
+    </a>
+    <h3>Rosy Janner</h3>
+    <p>Senior Finance Lecturer</p>
+    <div class="social-icons">
+     <a href="#">
+      <i class="fab fa-facebook-f"></i>
+     </a>
+     <a href="#">
+      <i class="fab fa-twitter"></i>
+     </a>
+     <a href="#">
+      <i class="fab fa-linkedin-in"></i>
+     </a>
+     <a href="#">
+      <i class="fab fa-google-plus-g"></i>
+     </a>
+    </div>
+   </div>
+
+   <div class="cot_t">
+    <a href="#" target="_blank">
+      <img height="300" width="200" src="T_Tho/img/cd_1.jpg" />
+      <div class="info">
+        <strong>Tên: John Doe</strong><br>
+        Ngày sinh: 01/01/1990<br>
+        Quê quán: Hà Nội
+      </div>
+    </a>
+    <h3>Rosy Janner</h3>
+    <p>Senior Finance Lecturer</p>
+    <div class="social-icons">
+     <a href="#">
+      <i class="fab fa-facebook-f"></i>
+     </a>
+     <a href="#">
+      <i class="fab fa-twitter"></i>
+     </a>
+     <a href="#">
+      <i class="fab fa-linkedin-in"></i>
+     </a>
+     <a href="#">
+      <i class="fab fa-google-plus-g"></i>
+     </a>
+    </div>
+   </div>
+
+   <div class="cot_t">
+    <a href="#" target="_blank">
+      <img height="300" width="200" src="T_Tho/img/cd_1.jpg" />
+      <div class="info">
+        <strong>Tên: John Doe</strong><br>
+        Ngày sinh: 01/01/1990<br>
+        Quê quán: Hà Nội
+      </div>
+    </a>
+    <h3>Rosy Janner</h3>
+    <p>Senior Finance Lecturer</p>
+    <div class="social-icons">
+     <a href="#">
+      <i class="fab fa-facebook-f"></i>
+     </a>
+     <a href="#">
+      <i class="fab fa-twitter"></i>
+     </a>
+     <a href="#">
+      <i class="fab fa-linkedin-in"></i>
+     </a>
+     <a href="#">
+      <i class="fab fa-google-plus-g"></i>
+     </a>
+    </div>
+   </div>
+</div>
+</div>
