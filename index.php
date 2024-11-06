dafa5cc01802bda60918a1c28df77551a3f2c0a6
NNTHCDBT <nnth.cdbt2024@gmail.com>
2024-11-06 15:24:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">				
+		<link  rel="stylesheet" href="H_Tho/footer.css"/>
+		<link rel="stylesheet" href="T_Tho/main.css">
+	</head>
+<body>
+<!-- <header> -->
+
+<!-- </header> -->
+
+<main>
+    <?php
+	    include'T_Tho/course.php';
+		include'T_Tho/lecturers.php';
+	?>
+
+</main>
+
+<!--<footer>-->
+	<?php
+		include 'H_Tho/footer.php';
+	?>
+<!--</footer -->
+</body>	
+</html>
