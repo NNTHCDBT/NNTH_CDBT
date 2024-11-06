eeea838667552303616f68849de2d74e9e35b8bf
NNTHCDBT <nnth.cdbt2024@gmail.com>
2024-11-06 15:20:13 +0700

    Add files via upload

---
+++ H_Tho/footer.php
@@ -0,0 +1,83 @@
+<html >
+ <head>
+ 
+  <title>
+	footer
+  </title>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
+   <link  rel="stylesheet" href="footer.css"/>
+ 
+ </head>
+ <body>
+<footer>
+ <footer>
+        <div class="container">
+            <div class="cot">
+                <div class="logo">
+                    <img src="H_Tho/img/logo.png" />
+                    <h3>Trường Cao Đẳng Bến tre </h3>
+                </div>
+              <p>Địa chỉ: ấp 1,xã Sơn Đông,Thành phố Bến Tre<br>
+			  <p>Điện đoại: 02753823492<br>
+			  <p>Fax: 075.3820.103<br>
+			  <p>Email: tuyensinhbtr@yahoo.com.vn</p>
+
+              <i id="first" class="fab fa-facebook-f"></i>
+              <i id="first" class="fab fa-twitter"></i>
+              <i id="first" class="fab fa-linkedin-in"></i>
+              <i id="first" class="fab fa-pinterest"></i>
+              <i id="first" class="fas fa-rss"></i>
+              <i id="first" class="fab fa-google-plus-g"></i>
+			
+                <div class="newsletter">
+                    <input placeholder="enter your e-mail here " type="email"/>
+                    <button>
+                        <i class="fas fa-paper-plane"></i>
+                    </button>
+                </div>
+             </div>
+
+             <div class="cot">
+              <h3>Truy cập nhanh</h3>
+				<ul>
+				<li> <a href="">Trang chủ  </a></li>
+				<li> <a href="">Giới thiệu </a></li>
+				<li> <a href="">Thông báo </a></li>
+				<li> <a href="">Hoạt động  </a></li>
+				<li> <a href="">Liên hệ  </a></li>
+				</ul>
+                
+             </div>
+
+            
+             <div class="cot">
+              <h3> Hình ảnh</h3>
+              <img height="100" src="H_Tho/img/Flickr 1.jpg" width="120"/>
+              <img height="100" src="H_Tho/img/Flickr 2.jpg" width="120"/>
+              <img height="100" src="H_Tho/img/Flickr 3.jpg" width="120"/>
+              <img height="100" src="H_Tho/img/Flickr 4.jpg" width="120"/>
+              <img height="100" src="H_Tho/img/Flick 5.jpg" width="120"/>
+              <img height="100" src="H_Tho/img/Flickr 6.jpg" width="120"/>
+             </div>
+
+            </div>
+            <div class="footer-bottom">
+                    <p>
+                      Copyright © by 
+ 
+                    <a href="#">
+                      Khoa NN-TH
+                    </a>
+                    </p>
+
+                    <div class="card">
+                     <img alt="Visa logo" height="30" src="H_Tho/img/Flickr 1.jpg" width="50"/>
+                     <img alt="Discover logo" height="30" src="H_Tho/img/Flickr 1.jpg" width="50"/>
+                     <img alt="PayPal logo" height="30" src="H_Tho/img/Flickr 1.jpg" width="50"/>
+                     <img alt="MasterCard logo" height="30" src="H_Tho/img/Flickr 1.jpg" width="50"/>
+                    </div>
+            </div>
+
+    </footer>
+ </body>
+ </html>
